dafc037790b529d5477cff00e51edf0877db2e35
Vladyslav Tupchiienko <vt.progr@gmail.com>
2021-11-24 13:12:58 +0200

    Added new exception to handler

--- src/main/java/com/example/schedule/controller/GlobalControllerExceptionHandler.java
+++ src/main/java/com/example/schedule/controller/GlobalControllerExceptionHandler.java
@@ -3,6 +3,7 @@ package com.example.schedule.controller;
 import com.example.schedule.data.response.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.UnsatisfiedServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,6 +12,7 @@ import java.sql.Timestamp;
 import java.time.Instant;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
@@ -35,6 +37,18 @@ public class GlobalControllerExceptionHandler {
                 HttpStatus.NOT_FOUND.value(),
                 Timestamp.from(Instant.now()),
                 List.of(e.getMessage())
+        );
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(UnsatisfiedServletRequestParameterException.class)
+    public ErrorResponse handleUnsatisfiedServletRequestParameterExceptions(
+            UnsatisfiedServletRequestParameterException e
+    ) {
+        return new ErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                Timestamp.from(Instant.now()),
+                List.of(Objects.requireNonNull(e.getMessage()))
         );
     }
 }
